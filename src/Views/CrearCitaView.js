f21bf3ea4aeb01ba6bb466665b238b9745e46681
Agustinsn <agustinasn95@gmail.com>
2021-06-25 01:42:33 -0500

    .

--- src/Views/CrearCitaView.js
+++ src/Views/CrearCitaView.js
@@ -9,16 +9,16 @@ function CrearCitaView() {
     return (
       <div className="container" style={{height:'100%'}}>
         <div className="row">
-        <div className="col-12 col-lg-10" style={{paddingTop:'50px',textAlign:'center'}}>
-          {boton1===true? 
-          <Form/>
-          : <div><EstadoCita/></div>}
-        </div>
-        <div className="col-12 col-lg-2" style={{marginTop:'50px'}}>
+        <div className="col-12 col-lg-2 order-md-1" style={{marginTop:'50px',textAlign:'end'}}>
         <button
           onClick={(e)=>setBoton1(!boton1)}
           style={{backgroundColor:'salmon'}}
           >Conozca el estado de su cita <strong>AQUI</strong></button>
+        </div>
+        <div className="col-12 col-lg-10 order-md-0" style={{paddingTop:'50px',textAlign:'center'}}>
+          {boton1===true? 
+          <Form/>
+          : <div><EstadoCita/></div>}
         </div>
         </div>
       </div>
